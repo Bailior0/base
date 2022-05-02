bbd35992f9ee6478798d25365e37570293b01ad8
Bailior0 <79334809+Bailior0@users.noreply.github.com>
2022-05-02 12:47:10 +0200

    Add files via upload

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -8,7 +8,9 @@ public class TrainSensorImpl implements TrainSensor {
 
 	private TrainController controller;
 	private TrainUser user;
-	private int speedLimit = 5;
+	private int speedLimit = 10;
+
+	//This is branch A and branch_B merged
 
 	public TrainSensorImpl(TrainController controller, TrainUser user) {
 		this.controller = controller;
