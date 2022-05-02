f57ca16b277f871f7fdb481184253817d3a089c5
Borbáth Eszter <61252275+eszter1999@users.noreply.github.com>
2022-05-02 15:31:13 +0200

    changing reference speed implemented with threads

---
+++ base-master/train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -0,0 +1,31 @@
+package hu.bme.mit.train.sensor;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+
+public class TrainSensorImpl implements TrainSensor {
+
+	private TrainController controller;
+	private TrainUser user;
+	private int speedLimit = 10;
+
+	//This is branch A and branch_B merged
+
+	public TrainSensorImpl(TrainController controller, TrainUser user) {
+		this.controller = controller;
+		this.user = user;
+	}
+
+	@Override
+	public int getSpeedLimit() {
+		return speedLimit;
+	}
+
+	@Override
+	public void overrideSpeedLimit(int speedLimit) {
+		this.speedLimit = speedLimit;
+		controller.setSpeedLimit(speedLimit);
+	}
+
+}
